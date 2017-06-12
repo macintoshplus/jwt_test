7f438d4911b11415cb8e268ac135467d50adacd4
macintoshplus <jean-baptiste@nahan.fr>
2017-06-12 07:36:37 +0200

    add comment

--- web/index.php
+++ web/index.php
@@ -9,9 +9,10 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
-
+//Instansite application object
 $app = new Application();
 $app->register(new \Silex\Provider\MonologServiceProvider(), ['monolog.logfile' => __DIR__.'/../logs/server.log']);
+// Configure security
 $app->register(new \Silex\Provider\SecurityServiceProvider(), ['security.firewalls' => [
         'api'=> [
             'pattern' => '/api/',
@@ -27,25 +28,30 @@ $app->register(new \Silex\Provider\SecurityServiceProvider(), ['security.firewal
             'anonymous' => false,
         ]
     ]]);
+//List of allowed issiuer can use the API.
 $app['clientKeyList'] = [
     'localhost' => ['pem'=>'file://'.__DIR__.'/../public.pem', 'roles'=>['ROLE_LIST', 'ROLE_ADD']],
 ];
+
+// Setup custom user provider
 $app['userProvider'] = (function ($app) {
     return new \JWT\Server\UserProvider($app['clientKeyList']);
 });
 
+// Setup custom Guard Authenticator
 $app['app.jwtoken_authenticator'] = function ($app) {
     return new \JWT\Security\JWTokenAuthenticator($app['logger']);
 };
 
+// Custom Route class for add trai security
 $app['route_class'] = 'JWT\MyRoute';
 
-
+//Load datas liste
 $liste = [['name'=> 'element 1'], ['name'=> 'element 2'], ['name'=> 'element 3']];
 if (file_exists(__DIR__.'/../datas.json')) {
     $liste = json_decode(file_get_contents(__DIR__.'/../datas.json'), true);
 }
-
+//Configure route with security
 $app->get('/api/list', function (Request $request) use (&$liste) {
     return new JsonResponse($liste);
 })->secure('ROLE_LIST');
@@ -59,6 +65,13 @@ $app->get('/api/add/{name}', function (Request $request, $name) use (&$liste) {
     return new JsonResponse($data);
 })->secure('ROLE_ADD');
 
-
+// Add custom error handler for return response in json format if need.
+$app->error(function (\Exception $e, Request $request, $code) {
+    if ($request->headers->has('Accept') && false !== strpos($request->headers->get('Accept'), 'application/json')) {
+        return new JsonResponse(['message'=>$e->getMessage()], $code);
+    }
+});
+//Run the application
 $app->run();
+//Save the datas into file
 file_put_contents(__DIR__.'/../datas.json', json_encode($liste));
