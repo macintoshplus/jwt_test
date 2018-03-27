b78ca4228999bfb674fe197bfa66412652d9ebdd
Jean-Baptiste NAHAN <jean-baptiste.nahan@inextenso.fr>
2018-03-27 16:55:39 +0200

    add more option

--- decodeJwt.php
+++ decodeJwt.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require __DIR__.'/vendor/autoload.php';
@@ -8,21 +9,58 @@ use Lcobucci\JWT\Signer\Keychain; // just to make our life simpler
 use Lcobucci\JWT\Signer\Key;
 use Lcobucci\JWT\Parser;
 
+if (php_sapi_name() !== 'cli') {
+    echo "This file can be used only in cli sapi.";
+    exit(255);
+}
+
+function displayHelp()
+{
+    global $argv;
+    echo "Usage : ".$argv[0].' <token_file> <public_key_file>'."\n";
+}
+
+
+if (count($argv) < 2) {
+    displayHelp();
+    exit(1);
+}
+
+$tokenFile = $argv[1];
+
+$publicKeyFile = $argv[2];
+
+if (!file_exists($tokenFile)) {
+    echo "Token file is not found : ".$tokenFile."\n\n";
+    displayHelp();
+    exit(1);
+}
+
+
+if (!file_exists($publicKeyFile)) {
+    echo "Public key file is not found : ".$publicKeyFile."\n\n";
+    displayHelp();
+    exit(1);
+}
+
+
 $keychain = new Keychain();
 
 $signer = new Sha256();
 
 
-$token = (new Parser())->parse(file_get_contents(__DIR__.'/token_593075af6c8c7')); // Parses from a string
+$token = (new Parser())->parse(file_get_contents($tokenFile)); // Parses from a string
 $token->getHeaders(); // Retrieves the token header
 $token->getClaims(); // Retrieves the token claims
 
-echo $token->getHeader('jti'), "\n"; // will print "4f1g23a12aa"
-echo $token->getClaim('iss'), "\n"; // will print "http://example.com"
-echo $token->getClaim('uid'), "\n"; // will print "1"
-echo $token, "\n"; // The string representation of the object is a JWT string (pretty easy, right?)
+echo "jti: ".$token->getHeader('jti'), "\n"; // will print "4f1g23a12aa"
+echo "iss: ".$token->getClaim('iss'), "\n"; // will print "http://example.com"
+echo "aud: ".$token->getClaim('aud'), "\n";
+echo "uid: ".$token->getClaim('uid'), "\n"; // will print "1"
+echo "Token: ".$token, "\n"; // The string representation of the object is a JWT string (pretty easy, right?)
 
-echo "alog : ".$token->getHeader('alg')." => ". $signer->getAlgorithmId()."\n";
+echo "algo: ".$token->getHeader('alg')." => ". $signer->getAlgorithmId()."\n";
 
-var_dump($token->verify($signer, new Key('file://'.__DIR__.'/public.pem')));
+echo "Result of verify: ";
+var_dump($token->verify($signer, new Key('file://'.$publicKeyFile)));
 echo "\n";
--- makeJwt.php
+++ makeJwt.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require __DIR__.'/vendor/autoload.php';
@@ -6,13 +7,64 @@ use Lcobucci\JWT\Builder;
 use Lcobucci\JWT\Signer\Rsa\Sha256;
 use Lcobucci\JWT\Signer\Key;
 
-$key = new Key('file://'.__DIR__.'/privkey.pem', 'toto');
-$pubKey = new Key('file://'.__DIR__.'/public.pem');
+if (php_sapi_name() !== 'cli') {
+    echo "This file can be used only in cli sapi.";
+    exit(255);
+}
+
+function displayHelp()
+{
+    global $argv;
+    echo "Usage : ".$argv[0].' <priv_key_file> <public_key_file>'."\n";
+}
+
+
+if (count($argv) < 2) {
+    displayHelp();
+    exit(1);
+}
+
+$privkeyFile = $argv[1];
+
+$publicKeyFile = $argv[2];
+
+if (!file_exists($privkeyFile)) {
+    echo "Private key file is not found : ".$privkeyFile."\n\n";
+    displayHelp();
+    exit(1);
+}
+
+
+if (!file_exists($publicKeyFile)) {
+    echo "Public key file is not found : ".$publicKeyFile."\n\n";
+    displayHelp();
+    exit(1);
+}
+
+$qh = new \Symfony\Component\Console\Helper\QuestionHelper();
+$q = new \Symfony\Component\Console\Question\Question("Type the pass phrase for the private key \"".$privkeyFile."\":\n");
+$q->setHidden(true);
+$q->setMaxAttempts(3);
+$output = new \Symfony\Component\Console\Output\ConsoleOutput();
+
+$input = new \Symfony\Component\Console\Input\ArgvInput();
+
+$secret = $qh->ask($input, $output, $q);
+
+$q = new \Symfony\Component\Console\Question\Question("Type the Issuer : \n");
+$issuer = $qh->ask($input, $output, $q);
+
+$q = new \Symfony\Component\Console\Question\Question("Type the Audience : \n");
+$audience = $qh->ask($input, $output, $q);
+
+
+$key = new Key('file://'.$privkeyFile, $secret);
+$pubKey = new Key('file://'.$publicKeyFile);
 
 $signer = new Sha256();
 
-$token = (new Builder())->setIssuer('http://example.com') // Configures the issuer (iss claim)
-                        ->setAudience('http://example.org') // Configures the audience (aud claim)
+$token = (new Builder())->setIssuer($issuer) // Configures the issuer (iss claim)
+                        ->setAudience($audience) // Configures the audience (aud claim)
                         ->setId('4f1g23a12aa', true) // Configures the id (jti claim), replicating as a header item
                         ->setIssuedAt(time()) // Configures the time that the token was issue (iat claim)
                         ->setNotBefore(time() + 60) // Configures the time that the token can be used (nbf claim)
@@ -20,17 +72,18 @@ $token = (new Builder())->setIssuer('http://example.com') // Configures the issu
                         ->set('uid', 1) // Configures a new claim, called "uid"
                         ->sign($signer, $key)
                         ->getToken(); // Retrieves the generated token
-
-file_put_contents(__DIR__.'/token_'.uniqid(), (string) $token);
-
+$file = __DIR__.'/token_'.uniqid();
+file_put_contents($file, (string) $token);
+echo "Token written in '".$file."'\n";
 
 $token->getHeaders(); // Retrieves the token headers
 $token->getClaims(); // Retrieves the token claims
 
-echo $token->getHeader('jti'), "\n"; // will print "4f1g23a12aa"
-echo $token->getClaim('iss'), "\n"; // will print "http://example.com"
-echo $token->getClaim('uid'), "\n"; // will print "1"
-echo $token, "\n"; // The string representation of the object is a JWT string (pretty easy, right?)
+echo "jti: ".$token->getHeader('jti'), "\n"; // will print "4f1g23a12aa"
+echo "iss: ".$token->getClaim('iss'), "\n"; // will print "http://example.com"
+echo "uid: ".$token->getClaim('uid'), "\n"; // will print "1"
+echo "Token: ".$token, "\n"; // The string representation of the object is a JWT string (pretty easy, right?)
 
+echo "Result of verify: ";
 var_dump($token->verify($signer, $pubKey));
 echo "\n";
